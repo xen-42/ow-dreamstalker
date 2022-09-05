ffcc036538e454086de836a8bfa9f5d9939b0ef9
Nick <nicholasconnors@hotmail.ca>
2022-09-05 17:38:54 -0400

    Dim the flashlight

---
+++ Dreamstalker/Patches/FlashlightPatches.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace Dreamstalker.Patches;
+
+[HarmonyPatch]
+internal class FlashlightPatches
+{
+	[HarmonyPostfix]
+	[HarmonyPatch(typeof(Flashlight), nameof(Flashlight.Start))]
+	public static void Flashlight_Start(Flashlight __instance)
+	{
+		foreach (var light in __instance._lights)
+		{
+			light.GetLight().color = new Color(0.1f, 0.2f, 0.1f, 1f);
+		}
+	}
+}
