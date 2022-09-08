5133170851c31520499e7ff11fbd0a3b79bd673a
Nick <nicholasconnors@hotmail.ca>
2022-09-07 23:43:08 -0400

    Keep flashlight strength in debug mode

--- Dreamstalker/Patches/FlashlightPatches.cs
+++ Dreamstalker/Patches/FlashlightPatches.cs
@@ -10,6 +10,8 @@ internal class FlashlightPatches
 	[HarmonyPatch(typeof(Flashlight), nameof(Flashlight.Start))]
 	public static void Flashlight_Start(Flashlight __instance)
 	{
+		if (Main.DebugMode) return;
+
 		foreach (var light in __instance._lights)
 		{
 			light.GetLight().color = new Color(0.1f, 0.2f, 0.1f, 1f);
