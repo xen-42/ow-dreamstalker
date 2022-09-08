826c9920fbfd8f132fa9092dad6992b1fa423473
Nick <nicholasconnors@hotmail.ca>
2022-09-08 00:24:32 -0400

    Remove weird usings

--- Dreamstalker/Components/DreamstalkerController.cs
+++ Dreamstalker/Components/DreamstalkerController.cs
@@ -1,12 +1,5 @@
-﻿using Dreamstalker.Handlers.SolarSystem;
-using Dreamstalker.Utility;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Dreamstalker.Utility;
 using UnityEngine;
-using static TimelineObliterationController;
 
 namespace Dreamstalker.Components;
 
