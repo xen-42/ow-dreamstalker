eec0a92bc93d2ce267f50379602b04a8edd0abdf
Nick <nicholasconnors@hotmail.ca>
2022-09-08 23:05:16 -0400

    Create completion volume (enter to go to next planet)

---
+++ Dreamstalker/Components/CompletionVolume.cs
@@ -0,0 +1,42 @@
+﻿using Dreamstalker.Utility;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Dreamstalker.Components;
+
+internal class CompletionVolume : MonoBehaviour
+{
+	public AstroObject.Name NextPlanet { set; private get; }
+
+	private Campfire _campfire;
+
+	public UnityEvent OnPlayerEnter = new();
+
+	public void SetCampfire(Campfire campfire)
+	{
+		_campfire = campfire;
+
+		campfire.OnCampfireStateChange += OnCampfireStateChange;
+	}
+
+	public void OnDestroy()
+	{
+		if (_campfire != null)
+		{
+			_campfire.OnCampfireStateChange -= OnCampfireStateChange;
+		}
+	}
+
+	private void OnCampfireStateChange(Campfire campfire) =>
+		enabled = campfire.GetState() == Campfire.State.LIT;
+
+	public void OnTriggerEnter(Collider hitCollider)
+	{
+		if (hitCollider.attachedRigidbody.CompareTag("Player"))
+		{
+			OnPlayerEnter?.Invoke();
+			PlayerEffectController.Instance.Blink(0f, 10f);
+			PlayerSpawnUtil.SpawnAt(NextPlanet);
+		}
+	}
+}
