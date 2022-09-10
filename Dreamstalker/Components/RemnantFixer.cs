7030ff1390dd8803cd225fc890991df2d73c46a6
Nick <nicholasconnors@hotmail.ca>
2022-09-10 19:44:48 -0400

    Less sunlight

--- Dreamstalker/Components/RemnantFixer.cs
+++ Dreamstalker/Components/RemnantFixer.cs
@@ -7,7 +7,9 @@ internal class RemnantFixer : MonoBehaviour
 	private void Awake()
 	{
 		gameObject.transform.Find("StellarRemnant").gameObject.SetActive(true);
-		gameObject.transform.Find("StellarRemnant/Star/SunLight").GetComponent<Light>().range = 20000;
+		var light = gameObject.transform.Find("StellarRemnant/Star/SunLight").GetComponent<Light>();
+		light.range = 20000;
+		light.intensity = 0.4f;
 		this.enabled = false;
 	}
 }
