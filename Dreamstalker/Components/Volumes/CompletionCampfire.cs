b6b88d65d219d4e344b23e2fdc79ae9c6287e10c
Nick <nicholasconnors@hotmail.ca>
2022-09-11 13:13:37 -0400

    Create campfire for Dreamworld

---
+++ Dreamstalker/Components/Volumes/CompletionCampfire.cs
@@ -0,0 +1,42 @@
+﻿using Dreamstalker.Utility;
+using UnityEngine;
+
+namespace Dreamstalker.Components.Volumes;
+
+public class CompletionCampfire : MonoBehaviour
+{
+	private Campfire _campfire;
+	private bool _playerSleepingHere;
+	public AstroObject.Name destinationPlanet;
+
+	public void Awake()
+	{
+		_campfire = gameObject.GetComponent<Campfire>();
+
+		GlobalMessenger<bool>.AddListener("StartSleepingAtCampfire", OnStartSleepingAtCampfire);
+		GlobalMessenger.AddListener("StopSleepingAtCampfire", OnStopSleepingAtCampfire);
+	}
+
+	public void OnDestroy()
+	{
+		GlobalMessenger<bool>.RemoveListener("StartSleepingAtCampfire", OnStartSleepingAtCampfire);
+		GlobalMessenger.RemoveListener("StopSleepingAtCampfire", OnStopSleepingAtCampfire);
+	}
+
+	public void OnStopSleepingAtCampfire()
+	{
+		if (_playerSleepingHere)
+		{
+			PlayerSpawnUtil.SpawnAt(destinationPlanet);
+		}
+
+		_playerSleepingHere = false;
+	}
+
+	public void OnStartSleepingAtCampfire(bool _)
+	{
+		_playerSleepingHere = _campfire._isPlayerSleeping;
+
+		// TODO: start loading in the destination planet
+	}
+}
