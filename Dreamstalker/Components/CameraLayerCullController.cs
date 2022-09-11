23d59041ac57388937a7cb756800ef11c761bf59
Nick <nicholasconnors@hotmail.ca>
2022-09-10 22:34:57 -0400

    Allow culling all distance objects but the sun

---
+++ Dreamstalker/Components/CameraLayerCullController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dreamstalker.Components;
+
+internal class FarClipPlaneController : SectoredMonoBehaviour
+{
+	private SectorDetector _playerSectorDetector;
+	private List<OWCamera> _affectedCameras = new();
+	private bool _containsPlayer;
+
+	public void Start()
+	{
+		_playerSectorDetector = Locator.GetPlayerSectorDetector();
+		GlobalMessenger<OWCamera>.AddListener("SwitchActiveCamera", OnSwitchActiveCamera);
+	}
+
+	public override void OnDestroy()
+	{
+		base.OnDestroy();
+
+		GlobalMessenger<OWCamera>.RemoveListener("SwitchActiveCamera", OnSwitchActiveCamera);
+	}
+
+	private void OnSwitchActiveCamera(OWCamera camera)
+	{
+		if (_containsPlayer)
+		{
+			AddCamera(camera);
+		}
+	}
+
+	private void AddCamera(OWCamera camera)
+	{
+		if (!_affectedCameras.Contains(camera))
+		{
+			_affectedCameras.Add(camera);
+
+			var distances = new float[32];
+			for (int i = 0; i < 32; i++)
+			{
+				// We set them to 3000f so we don't see that far, except the sun layer which stays at 0 meaning it takes the default farClipPlane
+				distances[i] = (i == LayerMask.NameToLayer("Sun") ? 0 : 3000f);
+			}
+			camera.mainCamera.layerCullDistances = distances;
+		}
+	}
+
+	public override void OnSectorOccupantAdded(SectorDetector sectorDetector)
+	{
+		base.OnSectorOccupantAdded(sectorDetector);
+
+		if (sectorDetector == _playerSectorDetector)
+		{
+			AddCamera(Locator.GetActiveCamera());
+
+			_containsPlayer = true;
+		}
+	}
+
+	public override void OnSectorOccupantRemoved(SectorDetector sectorDetector)
+	{
+		base.OnSectorOccupantRemoved(sectorDetector);
+
+		if (sectorDetector == _playerSectorDetector)
+		{
+			foreach (var cam in _affectedCameras)
+			{
+				cam.mainCamera.layerCullDistances = new float[32];
+			}
+			_affectedCameras.Clear();
+
+			_containsPlayer = false;
+		}
+	}
+}
