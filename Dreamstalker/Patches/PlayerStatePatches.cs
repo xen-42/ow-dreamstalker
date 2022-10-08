a03439044e397c73a731f3da4505f64e0145dd6a
Nick <nicholasconnors@hotmail.ca>
2022-10-08 15:59:19 -0400

    Always show flashlight prompt

---
+++ Dreamstalker/Patches/PlayerStatePatches.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dreamstalker.Patches;
+
+[HarmonyPatch(typeof(PlayerState))]
+internal class PlayerStatePatches
+{
+	[HarmonyPostfix]
+	[HarmonyPatch(nameof(PlayerState.Reset))]
+	private static void PlayerState_Reset()
+	{
+		PlayerState._inDarkZone = true;
+	}
+
+	// Always consider player to be in dark zone to keep flashlight prompt on
+	[HarmonyPostfix]
+	[HarmonyPatch(nameof(PlayerState.OnExitDarkZone))]
+	private static void PlayerState_OnExitDarkZone() => PlayerState._inDarkZone = true;
+}
