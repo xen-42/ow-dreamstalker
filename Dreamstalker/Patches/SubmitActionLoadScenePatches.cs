bfabfc688d59adfa1f19535936481aa53caf75e7
Nick <nicholasconnors@hotmail.ca>
2022-10-16 12:41:21 -0400

    Stop loading game scene before eye scene

--- Dreamstalker/Patches/SubmitActionLoadScenePatches.cs
+++ Dreamstalker/Patches/SubmitActionLoadScenePatches.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using NewHorizons.Patches;
 
 namespace Dreamstalker.Patches;
 
@@ -7,10 +8,16 @@ internal static class SubmitActionLoadScenePatches
 {
 	[HarmonyPostfix]
 	[HarmonyPatch(typeof(SubmitActionLoadScene), nameof(SubmitActionLoadScene.Update))]
+	[HarmonyPatch(typeof(SubmitActionLoadScene), nameof(SubmitActionLoadScene.ConfirmSubmit))]
 	private static void SubmitActionLoadScene_Update(SubmitActionLoadScene __instance)
 	{
-		// always redirect to eye lol
+		// Always redirect to the eye
 		if (__instance._sceneToLoad == SubmitActionLoadScene.LoadableScenes.GAME)
 			__instance._sceneToLoad = SubmitActionLoadScene.LoadableScenes.EYE;
 	}
+
+	// Stop NH from warping back to the eye
+	[HarmonyPrefix]
+	[HarmonyPatch(typeof(EyeOfTheUniversePatches), nameof(EyeOfTheUniversePatches.SubmitActionLoadScene_ConfirmSubmit))]
+	private static bool EyeOfTheUniversePatches_SubmitActionLoadScene_ConfirmSubmit() => false;
 }
