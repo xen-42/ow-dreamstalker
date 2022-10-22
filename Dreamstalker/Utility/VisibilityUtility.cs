a5f74b52a00df4407cd6c737c660e2ab3da3d02c
Nick <nicholasconnors@hotmail.ca>
2022-10-22 16:16:17 -0400

    Move visibility tracker creation to a utility class

---
+++ Dreamstalker/Utility/VisibilityUtility.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Dreamstalker.Utility;
+
+internal static class VisibilityUtility
+{
+	public static VisibilityTracker AddVisibilityTracker(Transform transform, float size)
+	{
+		var visibilityTracker = new GameObject("VisibilityTracker_Sphere");
+		visibilityTracker.transform.parent = transform;
+		visibilityTracker.transform.localPosition = Vector3.up * 0.5f;
+
+		var sphere = visibilityTracker.AddComponent<SphereShape>();
+		sphere.radius = size;
+
+		var tracker = visibilityTracker.AddComponent<ShapeVisibilityTracker>();
+
+		return tracker;
+	}
+}
