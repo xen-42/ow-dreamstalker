42277a9aa58fcddb06b8d9486a7d501c9b415cbf
Nick <nicholasconnors@hotmail.ca>
2022-10-23 01:41:13 -0400

    Hide launch code prompts

---
+++ Dreamstalker/Patches/KeyInfoPromptControllerPatches.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+
+namespace Dreamstalker.Patches;
+
+[HarmonyPatch(typeof(KeyInfoPromptController))]
+internal class KeyInfoPromptControllerPatches
+{
+	[HarmonyPostfix]
+	[HarmonyPatch(nameof(KeyInfoPromptController.Start))]
+	public static void KeyInfoPromptController_Start(KeyInfoPromptController __instance)
+	{
+		// Don't show launch codes ever
+		__instance._displayCodePrompt = false;
+	}
+}
