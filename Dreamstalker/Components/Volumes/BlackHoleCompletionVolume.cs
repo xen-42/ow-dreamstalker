b6fa8fa2b4e3cb931d8ca7d70d568a1dd888a2e5
Nick <nicholasconnors@hotmail.ca>
2022-10-26 21:46:21 -0400

    Fix BH randomly killing you

--- Dreamstalker/Components/Volumes/BlackHoleCompletionVolume.cs
+++ Dreamstalker/Components/Volumes/BlackHoleCompletionVolume.cs
@@ -13,17 +13,20 @@ internal class BlackHoleCompletionVolume : CompletionVolume
 		}
 	}
 
-	public override void OnTriggerEnter(Collider collider)
+	public override void OnTriggerEnter(Collider hitCollider)
 	{
 		if (!enabled)
 		{
-			// If not enabled, kill player when they enter the black hole
-			Locator.GetDeathManager().KillPlayer(DeathType.Impact);
-			PlayerEffectController.Instance.PlayOneShot(AudioType.BH_BlackHoleEmission);
+			if (hitCollider.attachedRigidbody == Locator.GetPlayerBody()._rigidbody)
+			{
+				// If not enabled, kill player when they enter the black hole
+				Locator.GetDeathManager().KillPlayer(DeathType.Impact);
+				PlayerEffectController.Instance.PlayOneShot(AudioType.BH_BlackHoleEmission);
+			}
 		}
 		else
 		{
-			base.OnTriggerEnter(collider);
+			base.OnTriggerEnter(hitCollider);
 		}
 	}
 }
