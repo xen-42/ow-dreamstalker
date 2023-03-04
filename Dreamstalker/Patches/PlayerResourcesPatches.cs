2c006db0d4dc17f789086dd0737cf9bafeaf4314
Nick <nicholasconnors@hotmail.ca>
2023-03-03 20:10:15 -0500

    Potentially fix asphyxia bug

---
+++ Dreamstalker/Patches/PlayerResourcesPatches.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+
+namespace Dreamstalker.Patches;
+
+[HarmonyPatch]
+internal class PlayerResourcesPatches
+{
+	[HarmonyPrefix]
+	[HarmonyPatch(typeof(PlayerResources), nameof(PlayerResources.IsOxygenPresent))]
+	private static bool PlayerResources_IsOxygenPresent(PlayerResources __instance, ref bool __result)
+	{
+		// Prevent random asphyxiation
+		__result = !__instance.IsUnderwater();
+		return false;
+	}
+}
