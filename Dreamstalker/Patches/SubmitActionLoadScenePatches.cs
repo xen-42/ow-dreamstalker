1c45ef7d00b6cf41af4e532461c7cce8cfcb2722
JohnCorby <williamforgames6@gmail.com>
2024-02-12 14:34:02 -0800

    fix for nh update

--- Dreamstalker/Patches/SubmitActionLoadScenePatches.cs
+++ Dreamstalker/Patches/SubmitActionLoadScenePatches.cs
@@ -17,5 +17,10 @@ internal static class SubmitActionLoadScenePatches
 	// Stop NH from warping back to the eye
 	[HarmonyPrefix]
 	[HarmonyPatch(typeof(NewHorizons.Patches.EyeScenePatches.SubmitActionLoadScenePatches), nameof(NewHorizons.Patches.EyeScenePatches.SubmitActionLoadScenePatches.SubmitActionLoadScene_ConfirmSubmit))]
-	private static bool EyeOfTheUniversePatches_SubmitActionLoadScene_ConfirmSubmit() => false;
+	private static bool EyeOfTheUniversePatches_SubmitActionLoadScene_ConfirmSubmit(out bool __result)
+	{
+		// replace patch with "return true" so it just runs the original method
+		__result = true;
+		return false;
+	}
 }
